8e88a70478d14fb05e2f0054d851c71aeed0d2c4
Cory House <housecor@gmail.com>
2024-04-09 13:02:45 -0500

    a

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,43 @@
-import "./App.css";
+import { useEffect, useState } from "react";
+import { Food } from "./types";
 
 function App() {
-  return <h1>Menu</h1>;
+  const [foods, setFoods] = useState<Food[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/foods").then((resp) => {
+      resp.json().then((data) => {
+        console.log(data);
+        setFoods(data);
+      });
+    });
+  }, []);
+
+  return (
+    <>
+      <h1>Menu</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Price</th>
+            <th>Description</th>
+            <th>Tags</th>
+          </tr>
+        </thead>
+        <tbody>
+          {foods.map((food) => (
+            <tr key={food.id}>
+              <td>{food.name}</td>
+              <td>{food.price}</td>
+              <td>{food.description}</td>
+              <td>{food.tags.join(", ")}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
 }
 
 export default App;

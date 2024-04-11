842bb023de492248483ebf14925d1b68eda76e1f
Cory House <housecor@gmail.com>
2024-04-11 10:57:07 -0500

    a

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,14 @@ import { App } from "./app";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      throwOnError: true,
+      retry: 0,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
